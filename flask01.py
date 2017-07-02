d45bc145169da38f6641574a50a6f302a3d4f535
09apple <415807795@qq.com>
2017-07-02 08:35:02 +0800

    0701

--- flask01.py
+++ flask01.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask import request
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello,%s!</h1>' % name
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run()
